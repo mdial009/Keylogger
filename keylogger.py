117bb1b46a4df8bad6161a7d8c49fe3fca0b6fa1
Madany Diallo <Madanyx@gmail.com>
2020-10-31 02:59:08 -0400

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,2 +1,16 @@
 import pynput
 from pynput.keyboard import Key, Listener
+
+
+def on_pressed(key):
+    print("{0} pressed".format(key))
+
+
+def on_released(key):
+    if key == Key.esc:
+        return False
+
+
+# functions called when a button is pressed and released.
+with Listener(on_pressed=on_pressed, on_released=on_released) as thelistener:
+    thelistener.join()
